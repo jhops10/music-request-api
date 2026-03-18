479a8c3294050d4f5cf282335ca25f0491b5ec3b
Joao Paulo <joaowais@gmail.com>
2026-03-18 18:37:12 -0300

    first commit

---
+++ src/main/java/com/jhops10/music_request_api/MusicRequestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.music_request_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRequestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicRequestApiApplication.class, args);
+	}
+
+}
