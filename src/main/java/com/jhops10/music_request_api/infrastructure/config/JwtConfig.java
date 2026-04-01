833f24ce0f81975602b71dc302a2249706a6ac5c
Joao Paulo <joaowais@gmail.com>
2026-03-31 22:31:17 -0300

    feat(auth): implementa geração e validação de JWT

---
+++ src/main/java/com/jhops10/music_request_api/infrastructure/config/JwtConfig.java
@@ -0,0 +1,18 @@
+package com.jhops10.music_request_api.infrastructure.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class JwtConfig {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+
+}
